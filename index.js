af66931958cdc161ea72e70f2963f3edff77f926
Allan Hortle <allan.hortle@blueflag.com.au>
2016-05-19 15:51:06 +1000

    add react props to config

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "extends": [
+    parser: 'babel-eslint',
+    extends: [
         "eslint:recommended",
         "plugin:react/recommended"
     ],
-    "plugins": [
+    plugins: [
         "react"
     ],
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
         },
     },
-    "env": {
-        "browser": true,
-        "node": true
+    env: {
+        browser: true,
+        node: true,
+        es6: true
     },
-    "rules": {
-        "strict": 0
+    rules: {
+        strict: 0,
+        'no-console': 0,
+        indent: ['error', 4, {
+            SwitchCase: 1 
+        }],
+
+        // React
+        'react/no-danger': 0,
+        'react/prop-types': [2, {
+            ignore: ['children']
+        }],
+        'react/display-name': 0,
+        'react/jsx-closing-bracket-location': [1, {
+            selfClosing: 'line-aligned',
+            nonEmpty: 'after-props'
+        }]
     }
 }

2c114fa2fe39221344261818bfeeda750ee0f776
Allan Hortle <allanhortle@gmail.com>
2017-01-12 08:58:14 +1100

    Upgrade to 2017 and stage 3, remove redundant object rest/spread

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var es2015 = require('babel-preset-es2015');
+var es2016 = require('babel-preset-es2016');
+var es2017 = require('babel-preset-es2017');
+var stage3 = require('babel-preset-stage-3');
 var react = require('babel-preset-react');
-var objectRestSpread = require('babel-plugin-transform-object-rest-spread');
 
 module.exports = {
     plugins: []
         .concat(es2015.plugins)
-        .concat(react.plugins)
-        .concat(objectRestSpread)
+        .concat(es2016.plugins)
+        .concat(es2017.plugins)
+        .concat(stage3.plugins)
+        .concat(react.plugins);
 }

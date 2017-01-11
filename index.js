a9eb7a6535e9a157bbd4f949efb62d675f25db70
Allan Hortle <allanhortle@gmail.com>
2017-01-12 09:56:36 +1100

    add babel-plugin-transform-runtime

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var es2015 = require('babel-preset-es2015');
 var es2016 = require('babel-preset-es2016');
 var es2017 = require('babel-preset-es2017');
-var stage3 = require('babel-preset-stage-3');
 var react = require('babel-preset-react');
+var stage3 = require('babel-preset-stage-3');
+var transformRuntime = require('babel-plugin-transform-runtime');
 
 module.exports = {
     plugins: []
         .concat(es2015.plugins)
         .concat(es2016.plugins)
         .concat(es2017.plugins)
-        .concat(stage3.plugins)
         .concat(react.plugins)
+        .concat(stage3.plugins)
+        .concat(transformRuntime.plugins)
 }

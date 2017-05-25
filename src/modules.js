78be40228b72c9bc1228d722b425c4679b47ca4e
Allan Hortle <allanhortle@gmail.com>
2017-05-25 17:20:20 +1000

    Fix order

--- src/modules.js
+++ src/modules.js
@@ -4,28 +4,29 @@ import fs from 'fs';
 import path from 'path';
 // import chalk from 'chalk';
 
-function recurseNodeModules(currentPath, depth = 0) {
+function recurseNodeModules(currentPath, depth) {
     const nodeModules = path.resolve(currentPath,'node_modules');
 
     // console.log('=>', nodeModules);
-    fs.readdir(nodeModules, (err, files) => {
-        if(files) {
-            files.forEach(file => {
+    if (fs.existsSync(nodeModules)) {
+        fs.readdirSync(nodeModules).forEach(file => {
 
-                const modulePath = path.resolve(nodeModules, file);
+            const modulePath = path.resolve(nodeModules, file);
 
-                const isLink = fs.lstatSync(modulePath).isSymbolicLink();
+            const isLink = fs.lstatSync(modulePath).isSymbolicLink();
 
-                if(isLink) {
-                    const pkg = path.resolve(modulePath, 'package.json');
-                    const version = JSON.parse(fs.readFileSync(pkg, 'utf8')).version;
-                    console.log(`${Array(depth).fill(' - ')}${file}:`, version);
-                    recurseNodeModules(modulePath, depth + 1);
+            if(isLink) {
+                const pkg = path.resolve(modulePath, 'package.json');
+                const version = JSON.parse(fs.readFileSync(pkg, 'utf8')).version;
+                console.log(`${[].concat(depth).fill('| ').join('')}${file}:`, version);
+
+                if(depth.indexOf(file) === -1) {
+                    recurseNodeModules(modulePath, depth.concat(file));
                 }
-            });
-        }
-    });
+            }
+        });
+    }
 }
 export default function modules(program, config) {
-    recurseNodeModules('.');
+    recurseNodeModules('.', []);
 }

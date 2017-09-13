8f55255fb378c828a29b99a3c2c0ced0f1ddf4cb
Allan Hortle <allanhortle@gmail.com>
2017-09-13 15:45:45 +1000

    Add some basic tests

--- src/coverage.js
+++ src/coverage.js
@@ -3,34 +3,45 @@
 
 import ChildProcess from 'child_process';
 
+
 export default function Coverage(program: Object) {
     const {minCoverage = 80} = program;
     const cwd = process.cwd();
+
+    let nycBin = `${cwd}/node_modules/proto-blueflag-test/node_modules/.bin/nyc`;
+    let blueflagTestBin = `${cwd}/node_modules/.bin/blueflag-test`;
+
+    // blueflag-test is running itself
+    if(cwd.indexOf('proto-blueflag-test') !== -1) {
+        nycBin = `${cwd}/node_modules/nyc/bin/nyc.js`;
+        blueflagTestBin = `${cwd}/scripts/run`;
+    }
+
+
     const args = [
-        '--all',
-        // '--instrument=false',
-        '--check-coverage',
+        `--all`,
+        `--check-coverage`,
         `--branches=${minCoverage}`,
         `--functions=${minCoverage}`,
         `--lines=${minCoverage}`,
         `--statements=${minCoverage}`,
-        '--include=src/**',
-        '--reporter=text',
-        '--reporter=lcov',
-        '--extension=.jsx',
-        '--extension=.js',
-        `${cwd}/node_modules/.bin/blueflag-test`,
-        'test'
+        `--include=src/**`,
+        `--exclude=**/*-test.js`,
+        `--reporter=text`,
+        `--reporter=lcov`,
+        `--extension=.jsx`,
+        `--extension=.js`,
+        blueflagTestBin,
+        `test`
     ];
 
-    const command = `${cwd}/node_modules/proto-blueflag-test/node_modules/.bin/nyc`;
-
     ChildProcess
-        .spawn(command, args, {
+        .spawn(nycBin, args, {
             stdio: 'inherit',
             cwd: process.cwd()
         })
-        .on('exit', process.exit);
+        .on('exit', process.exit)
+        .on('error', err => console.log(err));
 }
 
 
--- src/index-test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import test from 'ava';
-import protoblueflagtest from './index';
-
-test('protoblueflagtest does the right thing', tt => {
-    tt.is(protoblueflagtest(123), 246, 'protoblueflagtest should add 123');
-    tt.is(protoblueflagtest(), 123, 'protoblueflagtest should return 123 when given no arguments');
-});

65c29c7a87fe9b4c1ed339403d45968176b1af26
Allan Hortle <allanhortle@gmail.com>
2018-07-06 14:12:42 +1000

    feat: add flow-coverage command

---
+++ src/flowCoverage.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+// @flow
+
+import flowCoverageReport from 'flow-coverage-report';
+import FlowBin from 'flow-bin';
+
+export default function FlowCoverage(program: *) {
+    const {monorepo = false} = program;
+    const {minCoverage = 50} = program;
+
+    const config = {
+        reportTypes: ['text', 'html'],
+        flowCommandPath: FlowBin,
+        projectDir: process.cwd(),
+        globExcludePatterns: [
+            '**/node_modules/**',
+            '**/lib/**',
+            '**/dist/**',
+            'packages/*-docs/**'
+        ],
+        globIncludePatterns: monorepo
+            ? ['packages/**/*.{js,jsx}']
+            : ['src/**/*.{js,jsx}'],
+        threshold: minCoverage,
+        outputDir: './flow-coverage',
+        concurrentFiles: 1,
+        strictCoverage: false,
+        excludeNonFlow: false,
+        noConfig: false,
+        htmlTemplateOptions: {
+            autoHeightSource: true,
+            showMeterBar: false
+        }
+    };
+    flowCoverageReport(config)
+        .catch(err => console.error(err));
+}

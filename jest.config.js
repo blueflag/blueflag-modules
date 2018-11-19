935d042c5e3725cd116c882ffbe4e753edfe30de
Allan Hortle <allanhortle@gmail.com>
2018-11-20 08:47:04 +1100

    test: coverage on the coverage tests

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    collectCoverage: true,
+    //collectCoverageFrom: [
+        //"(src|packages)/**/*.{js,jsx}",
+        //"!**/lib/**",
+        //"!**/node_modules/**"
+    //],
+    coverageReporters: ["json", "lcov", "text-summary"]
+};
--- src/__tests__/coverage-test.js
+++ src/__tests__/coverage-test.js
@@ -16,22 +16,22 @@ test('coverage has different args for monorepos', () => {
     return coverage({monorepo: true})
         .then(() => {
             expect(spawn).toHaveBeenCalled();
-            expect(spawn.firstCall.args[1][7]).toBe('--include=packages');
-            expect(spawn.firstCall.args[1][15]).toBe('--exclude=packages/*-docs');
+            expect(spawn.mock.calls[0][1][7]).toBe('--include=packages');
+            expect(spawn.mock.calls[0][1][15]).toBe('--exclude=packages/*-docs');
         });
 });
 
-//test('coverage has different args for normal repos', () => {
-    //return coverage({})
-        //.then(() => {
-            //expect(spawn.firstCall.args[1][7]).toBe('--include=src');
-            //expect(spawn.firstCall.args[1][15]).not.toBe('--exclude=packages/*-docs');
-        //});
-//});
+test('coverage has different args for normal repos', () => {
+    return coverage({})
+        .then(() => {
+            expect(spawn.mock.calls[1][1][7]).toBe('--include=src');
+            expect(spawn.mock.calls[1][1][15]).not.toBe('--exclude=packages/*-docs');
+        });
+});
 
-//test('coverage can take an custom test command', () => {
-    //return coverage({testCommand: ['foo']})
-        //.then(() => {
-            //expect(spawn.firstCall.args[1][15]).toBe('foo');
-        //});
-//});
+test('coverage can take an custom test command', () => {
+    return coverage({testCommand: ['foo']})
+        .then(() => {
+            expect(spawn.mock.calls[2][1][15]).toBe('foo');
+        });
+});

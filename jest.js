0db8248ec4c4bcc321674e2ef51748aa1e5f2fd0
Allan Hortle <allanhortle@gmail.com>
2018-12-10 08:31:33 +1100

    add: jest config and dependencies

---
+++ jest.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "plugins": [
+        "jest"
+    ],
+    "env": {
+        "jest": true
+    },
+    "rules": {
+        "jest/no-disabled-tests": "error",
+        "jest/no-focused-tests": "error"   
+    }
+};
